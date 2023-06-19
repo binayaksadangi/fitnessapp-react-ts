7cf62343c2c6e438fa2694d3addc9d89f9a67996
binayaksadangi <youbinayak@gmail.com>
2023-06-20 00:26:19 +0530

    tailwind config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import path from 'path'
+//import comp from "../../../src/componets/comp"
+//import comp from "@/components/comp"
+//path helps to find the folder when we use @
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{find: '@',replacement: path.resolve(__dirname, "src")}]
+  }
 })
