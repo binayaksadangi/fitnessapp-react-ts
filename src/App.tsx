43b1d6e540018902ae79cf1b88aa49fdc2e62fdb
binayaksadangi <youbinayak@gmail.com>
2023-06-20 17:28:05 +0530

    enums added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import Navbar from "@/scenes/Navbar";
 import { useState } from "react";
+
+import { SelectedPage } from "./shared/types";
+
 function App() {
-  const [selectedPage, setSelectedPage] = useState("home");
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
   return (
     <div className="app bg-gray-20">
       <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
---
+++ src/shared/types.ts
@@ -0,0 +1,6 @@
+export enum SelectedPage {
+    Home = "home",
+    Benifits = "benifits",
+    OurClasses = "ourClasses",
+    ContactUs = "contactUs",
+  }
